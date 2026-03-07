a43bd6a0fe5b1fdb2e80185cba52b1b2b1f00106
MoSub2001 <117198973+MoSub2001@users.noreply.github.com>
2026-03-07 16:53:29 +0200

    Add separate manager and customer booking calendars

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -3,14 +3,13 @@ import CalendarBox from "@/components/CalenderBox";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Calender Page",
-  // other metadata
+  title: "Booking Management Calendar",
 };
 
 const CalendarPage = () => {
   return (
     <>
-      <Breadcrumb pageName="Calendar" />
+      <Breadcrumb pageName="Booking Management" />
 
       <CalendarBox />
     </>
